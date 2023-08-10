e55a36c5b51cec90cbae5d1cb58c175b471def17
skybedy <skybedy@gmail.com>
2023-08-10 23:46:19 +0200

    Dalsi informace

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -97,8 +97,6 @@ nav li a{
 <body>
     <div class="container-fluid bg-black px-0 text-light">
 
-
-
         <div class="pt-2" style="border-bottom: 3px solid #fff;">
 
             <div class="row">
@@ -108,19 +106,18 @@ nav li a{
                     </a>
                 </div>
                 <div class="col">
-                    <h1 class="text-center">Tvá cesta k módě</h1>
+                    <h1 class="text-center">TVÁ CESTA K MÓDĚ</h1>
 
                 </div>
                 <div class="col">
                     <div>
                         <div class="row">
                             <div class="col"></div>
-                            <div class="col"></div>
                             <div class="col pb-0">
                                 <address>
                                     ADRESA:<br>
                                     Sokolská 789/1<br>
-                                    779 00 Olomouc
+                                    779 00 Olomouc (za SPEA)
                                 </address>
                             </div>
                         </div>
@@ -152,189 +149,123 @@ nav li a{
       
      
 <div class="container">
-
-
-
-
-
-      
-
-
-
     <div id="carouselExampleSlidesOnly" class="carousel slide border border-dark mt-3" data-ride="carousel">
         <div class="carousel-inner">
-          <div class="carousel-item active">
+        <div class="carousel-item active">
             <img class="d-block w-100" src="images/1.jpg" alt="First slide">
-          </div>
-          <div class="carousel-item">
+        </div>
+        <div class="carousel-item">
             <img class="d-block w-100" src="images/2.jpg" alt="Second slide">
-          </div>
-          <div class="carousel-item">
+        </div>
+        <div class="carousel-item">
             <img class="d-block w-100" src="images/3.jpg" alt="Third slide">
-          </div>
         </div>
-      </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-  <hr>
-
-<h2 class="h1 mt-5 text-center">Nabízíme k prodeji</h2>
-<div class="container mt-1">
-    <div class="row">
-        <div class="col-md-3">
-            <div class="card">
-                <img src="images/rolaky.jpg" class="card-img-top" alt="Obrázek 1">
-            </div>
-            <h5 class="text-center mt-4">Roláky</h5>
-        </div>
-        <div class="col-md-3">
-            <div class="card">
-                <img src="images/svetry.jpg" class="card-img-top" alt="Obrázek 2">
-            </div>
-            <h5 class="text-center mt-4">Svetry</h5>
-        </div>
-        <div class="col-md-3">
-            <div class="card">
-                <img src="images/vesty.jpg" class="card-img-top" alt="Obrázek 3">
-            </div>
-            <h5 class="text-center mt-4">Vesty</h5>
-        </div>
-        <div class="col-md-3">
-            <div class="card">
-                <img src="images/bundy.jpg" class="card-img-top" alt="Obrázek 3">
-            </div>
-            <h5 class="text-center mt-4">Bundy</h5>
         </div>
     </div>
-</div>
-
-<div class="container mt-3">
-    <div class="row">
-        <div class="col-md-3">
-            <div class="card">
-                <img src="images/teplaky.jpg" class="card-img-top" alt="Obrázek 1">
+    <hr>
+    <h2 class="h1 mt-5 text-center">Nabízíme k prodeji</h2>
+    <div class="container mt-1">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="card">
+                    <img src="images/rolaky.jpg" class="card-img-top" alt="Obrázek 1">
+                </div>
+                <h5 class="text-center mt-4">Roláky</h5>
             </div>
-            <h5 class="text-center mt-4">Tepláky</h5>
-        </div>
-        <div class="col-md-3">
-            <div class="card">
-                <img src="images/rifle.jpg" class="card-img-top" alt="Obrázek 2">
+            <div class="col-md-3">
+                <div class="card">
+                    <img src="images/svetry.jpg" class="card-img-top" alt="Obrázek 2">
+                </div>
+                <h5 class="text-center mt-4">Svetry</h5>
             </div>
-            <h5 class="text-center mt-4">Rifle</h5>
-        </div>
-        <div class="col-md-3">
-            <div class="card">
-                <img src="images/kabele.jpg" class="card-img-top" alt="Obrázek 3">
+            <div class="col-md-3">
+                <div class="card">
+                    <img src="images/vesty.jpg" class="card-img-top" alt="Obrázek 3">
+                </div>
+                <h5 class="text-center mt-4">Vesty</h5>
             </div>
-            <h5 class="text-center mt-4">Kabele</h5>
-        </div>
-        <div class="col-md-3">
-            <div class="card">
-                <img src="images/pyzama.jpg" class="card-img-top" alt="Obrázek 3">
-            </div>
-            <h5 class="text-center mt-4">Pyžama</h5>
-        </div>
-    </div>
-</div>
-<hr>
-
-<h2 class="h1 mt-5 text-center">Oblíbené značky</h2>
-
-
-<div class="container mt-3 mb-3">
-    <div class="row">
-        <div class="col-md-2">
-            <div class="card">
-                <img src="images/pietro-moneti-logo.jpg" class="card-img-top" alt="Obrázek 1">
-            </div>
-        </div>
-        <div class="col-md-2">
-            <div class="card">
-                <img src="images/vamp-logo.jpg" class="card-img-top" alt="Obrázek 2">
-            </div>
-        </div>
-        <div class="col-md-2">
-            <div class="card">
-                <img src="images/scharf-logo.jpg" class="card-img-top" alt="Obrázek 3">
-            </div>
-        </div>
-        <div class="col-md-2">
-            <div class="card">
-                <img src="images/ferrini-logo.jpg" class="card-img-top" alt="Obrázek 3">
-            </div>
-        </div>
-        <div class="col-md-2">
-            <div class="card">
-                <img src="images/cars-jeans-logo.jpg" class="card-img-top" alt="Obrázek 3">
-            </div>
-        </div>
-        <div class="col-md-2">
-            <div class="card">
-                <img src="images/cars-jeans-logo.jpg" class="card-img-top" alt="Obrázek 3">
+            <div class="col-md-3">
+                <div class="card">
+                    <img src="images/bundy.jpg" class="card-img-top" alt="Obrázek 3">
+                </div>
+                <h5 class="text-center mt-4">Bundy</h5>
             </div>
         </div>
     </div>
-</div>
 
-
-
-
-</div>
-
-
-
-
-
-
-
-   
-   
-   
-   
-
-
-
-
-
-
-        
-        
-        
-        
-                
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    <div class="container mt-3">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="card">
+                    <img src="images/teplaky.jpg" class="card-img-top" alt="Obrázek 1">
+                </div>
+                <h5 class="text-center mt-4">Tepláky</h5>
+            </div>
+            <div class="col-md-3">
+                <div class="card">
+                    <img src="images/rifle.jpg" class="card-img-top" alt="Obrázek 2">
+                </div>
+                <h5 class="text-center mt-4">Rifle</h5>
+            </div>
+            <div class="col-md-3">
+                <div class="card">
+                    <img src="images/kabele.jpg" class="card-img-top" alt="Obrázek 3">
+                </div>
+                <h5 class="text-center mt-4">Kabele</h5>
+            </div>
+            <div class="col-md-3">
+                <div class="card">
+                    <img src="images/pyzama.jpg" class="card-img-top" alt="Obrázek 3">
+                </div>
+                <h5 class="text-center mt-4">Pyžama</h5>
+            </div>
+        </div>
     </div>
+    <hr>
 
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>       
+    <h2 class="h1 mt-5 text-center">Oblíbené značky</h2>
+    <div class="container mt-3 mb-3">
+        <div class="row">
+            <div class="col-md-2">
+                <div class="card">
+                    <img src="images/pietro-moneti-logo.jpg" class="card-img-top" alt="Obrázek 1">
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="card">
+                    <img src="images/vamp-logo.jpg" class="card-img-top" alt="Obrázek 2">
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="card">
+                    <img src="images/scharf-logo.jpg" class="card-img-top" alt="Obrázek 3">
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="card">
+                    <img src="images/ferrini-logo.jpg" class="card-img-top" alt="Obrázek 3">
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="card">
+                    <img src="images/cars-jeans-logo.jpg" class="card-img-top" alt="Obrázek 3">
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="card">
+                    <img src="images/cars-jeans-logo.jpg" class="card-img-top" alt="Obrázek 3">
+                </div>
+            </div>
+        </div>
+    </div>
+    <hr>
+    <div class="container mt-4">
+        <p>U nás najdete módu na volný čas, běžné nošení i sportovně elegantní pro každého muže.<br> Nabízíme Rifle zn. Cars Jeans, košile, polokošile, trika, mikiny, svetry, vesty, bundy, sportovní saka, různé kalhoty, kraťasy, čepice i bekovky, spodní prádlo, župany, plavky.
+        Jde většinou o přírodní a kvalitní materiály českých i zahraničních firem za skvělé ceny.<br> Těšíme se na vás, rády poradíme s výběrem a vhodným oblečením i velikostí.</p>
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>       
 </body>
 </html>
